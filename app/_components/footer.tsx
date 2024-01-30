1f345a00d4d8b711dcd21a7b6eba074eec3b1b43
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-01-30 14:10:37 -0300

    feat: Add Footer component to layout

---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="w-full bg-secondary py-6 px-5 mt-[4.5rem]">
+            <p className="text-gray-400 text-xs">© 2023 Copyright FSW Barber</p>
+        </div>
+    )
+}
+
+export default Footer
