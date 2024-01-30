caccbf89de97ddd6c372001d07a33b6f1bdb5536
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-01-30 11:01:34 -0300

    feat: Add initial search component to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,24 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
+    return (
+        <div>
+            <Header />
 
-      <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá Vitor!</h2>
-        <p className="capitalize text-sm">
-          {format(new Date(), "EEEE',' dd 'de' MMMM", {
-            locale: ptBR,
-          })}
-        </p>
-      </div>
-    </div>
-  );
+            <div className="px-5 pt-5">
+                <h2 className="text-xl font-bold">Olá Vitor!</h2>
+                <p className="capitalize text-sm">
+                    {format(new Date(), "EEEE',' dd 'de' MMMM", {
+                        locale: ptBR,
+                    })}
+                </p>
+            </div>
+            <div className="px-5 mt-6">
+                <Search />
+            </div>
+        </div>
+    );
 }
