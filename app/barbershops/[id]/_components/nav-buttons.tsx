6c32c2ea7e3c60d4e53281f9ea8983a41bd94b5c
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-01-31 09:35:34 -0300

    feat: Add NavButtons component for barbershop services and information

---
+++ app/barbershops/[id]/_components/nav-buttons.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { Button } from "@/app/_components/ui/button"
+import ServiceItem from "./service-item"
+import { Barbershop, Service } from "@prisma/client"
+import { useState } from "react"
+import { cn } from "@/app/_lib/utils"
+
+interface NavButtonsProps {
+    barbershop: {
+        services: Service[]
+    } & Barbershop
+
+}
+
+const NavButtons = ( {barbershop}: NavButtonsProps ) => {
+    const [activeTab, setActiveTab] = useState<"services" | "info">("services")
+
+    function changeTab(tab: "services" | "info") {
+        setActiveTab(tab)
+    }
+
+    return (
+        <div className="px-5 pt-6 flex flex-col gap-4">
+            <div className="flex items-center gap-3">
+                <Button variant={"outline"} className={cn(activeTab === 'services' ? 'bg-primary' : '')} onClick={() => changeTab('services')}>Serviços</Button>
+                <Button variant={"outline"} className={cn(activeTab === 'info' ? 'bg-primary' : '')} onClick={() => changeTab('info')}>Informações</Button>
+            </div>
+            {activeTab === 'services' ? barbershop.services.map(service => (
+                <ServiceItem key={service.id} service={service} />
+            )) : <span className="text-sm text-gray-400">Em breve!</span>}
+        </div>
+    )
+}
+
+export default NavButtons
