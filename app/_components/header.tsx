10f1b6e067b35815b8cd4f33e364ee2ec54c12fd
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-01-31 08:07:24 -0300

    Add link to logo in header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <Card>
             <CardContent className="flex justify-between items-center py-6 px-5">
-                <Image src="/logo.png" alt="logo" width={120} height={22} />
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={120} height={22} />
+                </Link>
                 <Button variant="outline" size="icon" className="h-8 w-8">
                     <MenuIcon size={18} />
                 </Button>
