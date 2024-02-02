4954382a20369cef6e1e0369d4498fc6b1824a9e
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-02-02 10:00:42 -0300

    feat: Refactor bookings page layout

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -45,27 +45,36 @@ export default async function BookingsPage() {
             <div className="px-5 py-6">
                 <h1 className="text-xl font-bold">Agendamentos</h1>
 
-                {confirmedBookings.length > 0 && (
+                {confirmedBookings.length === 0 && finishedBookings.length === 0 ? (
+                    <div className="p-6 bg-card rounded-lg shadow-lg mt-6">
+                        <p className="text-center text-gray-400">Você ainda não tem agendamentos</p>
+                    </div>
+                ) : 
                     <>
-                        <h2 className="text-sm uppercase text-gray-400 font-bold mt-6 mb-3">Confirmados</h2>
-                        <div className="space-y-3">
-                            {confirmedBookings.map((booking) => (
-                                <BookingItem key={booking.id} booking={booking} />
-                            ))}                
-                        </div>
-                    </>
-                )}
+                        {confirmedBookings.length > 0 && (
+                            <>
+                                <h2 className="text-sm uppercase text-gray-400 font-bold mt-6 mb-3">Confirmados</h2>
+                                <div className="space-y-3">
+                                    {confirmedBookings.map((booking) => (
+                                        <BookingItem key={booking.id} booking={booking} />
+                                    ))}                
+                                </div>
+                            </>
+                        )}
 
-                {finishedBookings.length > 0 && (
-                    <>
-                        <h2 className="text-sm uppercase text-gray-400 font-bold mt-6 mb-3">Finalizados</h2>
-                        <div className="space-y-3">
-                            {finishedBookings.map((booking) => (
-                                <BookingItem key={booking.id} booking={booking} />
-                            ))}                
-                        </div>
+                        {finishedBookings.length > 0 && (
+                            <>
+                                <h2 className="text-sm uppercase text-gray-400 font-bold mt-6 mb-3">Finalizados</h2>
+                                <div className="space-y-3">
+                                    {finishedBookings.map((booking) => (
+                                        <BookingItem key={booking.id} booking={booking} />
+                                    ))}                
+                                </div>
+                            </>
+                        )}
                     </>
-                )}
+                }
+
 
             </div>
         </>
