02b9388feb53fd66a1e2a36ffc479afc75857252
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-02-02 00:20:29 -0300

    Refactor BookingsPage to separate confirmed and finished bookings

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -11,18 +11,33 @@ export default async function BookingsPage() {
 
     if(!session?.user) return redirect("/");
 
-    const bookings = await db.booking.findMany({
-        where: {
-            userId: (session.user as any).id
-        },
-        include: {
-            service: true,
-            barbershop: true
-        }
-    })
+    const [confirmedBookings, finishedBookings] = await Promise.all([
+        db.booking.findMany({
+            where: {
+                userId: (session.user as any).id,
+                date: {
+                    gte: new Date()
+                }
+            },
+            include: {
+                service: true,
+                barbershop: true
+            }
+        }),
 
-    const confirmedBookings = bookings.filter(booking => isFuture(booking.date));
-    const finishedBookings = bookings.filter(booking => isPast(booking.date));
+        db.booking.findMany({
+            where: {
+                userId: (session.user as any).id,
+                date: {
+                    lt: new Date()
+                }
+            },
+            include: {
+                service: true,
+                barbershop: true
+            }
+        })
+    ]);
 
     return (
         <>
