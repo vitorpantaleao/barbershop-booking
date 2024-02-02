8bf041cdf90997391c3c779bb2ecbb48986d8c1b
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-02-02 09:50:15 -0300

    feat: Update greeting message in Home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
         <div>
             <Header />
             <div className="px-5 pt-5">
-                <h2 className="text-xl font-bold">Olá, Vitor!</h2>
+                <h2 className="text-xl font-bold">{session?.user ? `Olá, ${session.user.name?.split(' ')[0]}!` : "Olá, vamos agendar hoje?"}</h2>
                 <p className="capitalize text-sm">
                     {format(new Date(), "EEEE',' dd 'de' MMMM", {
                         locale: ptBR,
