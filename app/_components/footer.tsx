12cdcb0f4063036d1a7df9ceaa2d015025bb6c69
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-02-02 10:00:34 -0300

    Update footer margin in footer component

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="w-full bg-secondary py-6 px-5 mt-[4.5rem]">
+        <div className="w-full bg-secondary py-6 px-5 mt-[3rem]">
             <p className="text-gray-400 text-xs">© 2023 Copyright FSW Barber</p>
         </div>
     )
