06cb6f663ffd8767aec1f78e0fa657d57cb05701
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-02-03 02:10:13 -0300

    fix: Fix date selection bug in ServiceItem component

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -10,7 +10,7 @@ import { signIn, useSession } from "next-auth/react";
 import Image from "next/image"
 import { useEffect, useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
-import { format, setHours, setMinutes } from "date-fns";
+import { addDays, format, setHours, setMinutes } from "date-fns";
 import { saveBooking } from "../_actions/save-booking";
 import { Loader2 } from "lucide-react";
 import { toast } from "sonner"
@@ -147,7 +147,7 @@ const ServiceItem = ({service, isAuthenticated, barbershop}: ServiceItemProps) =
                                             selected={date}
                                             onSelect={handleDateSelect}
                                             locale={ptBR}
-                                            fromDate={new Date()}
+                                            fromDate={addDays(new Date(), 1)}
                                             styles={{
                                                 head_cell:{
                                                     width: "100%",
